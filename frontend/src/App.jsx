5e8a47b58695af838fdbb13e499da040e1a669f9
halannale <halanna.le@ucalgary.ca>
2026-03-18 15:20:25 -0600

    added login page - does not connect to database yet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import SignupPage from "./Pages/SignupPage";
+import LoginPage from "./Pages/LoginPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/home" element={<HomePage />} />
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/Pages/LoginPage.jsx
@@ -0,0 +1,65 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import "./LoginPage.css";
+
+function LoginPage() {
+    const navigate = useNavigate();
+    
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // temporary: just log values
+        console.log("Email:", email);
+        console.log("Password:", password);
+
+        // later: connect to backend
+        // for now, redirect
+        navigate("/home");
+    };
+
+    return (
+        <div className="loginpage">
+    
+          {/* ── Navbar ── */}
+          <nav className="navbar">
+            <div className="navbar-logo">
+    
+              <span className="logo-text">WHAT'S IN MY FRIDGE?</span>
+            </div>
+          </nav>
+    
+          {/* ── Login Box── */}
+          <section className="login">
+            <div className="login-content">
+              <h1 className="login-heading">Login</h1>
+              <form className="login-form" onSubmit={handleSubmit}>
+                <label>Email</label>
+                <input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <label>Password</label>
+                <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <button className="login-btn" type="submit">
+                    Login
+                </button>
+              </form>
+            </div>
+          </section>
+        </div>
+      );
+    }
+    
+    export default LoginPage;
