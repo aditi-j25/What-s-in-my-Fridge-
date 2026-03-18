549212915a5b4661562a1bb70684115e46d5cb6e
halannale <halanna.le@ucalgary.ca>
2026-03-18 16:07:10 -0600

    added sign up page - does not connect to database yet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
+import SignupPage from "./Pages/SignupPage";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<HomePage />} />
         <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/Pages/SignupPage.jsx
@@ -0,0 +1,87 @@
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import "./SignupPage.css";
+
+function SignupPage() {
+    const navigate = useNavigate();
+    
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState(""); // new state
+    const [error, setError] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setError("");
+
+        if (password !== confirmPassword) {
+            setError("**Passwords do not match**");
+            return;
+        }
+
+        // temporary: just log values
+        console.log("Email:", email);
+        console.log("Password:", password);
+
+        // later: connect to backend
+        // for now, redirect
+        navigate("/home");
+    };
+
+    return (
+        <div className="signuppage">
+    
+          {/* ── Navbar ── */}
+          <nav className="navbar">
+            <div className="navbar-logo">
+    
+              <span className="logo-text">WHAT'S IN MY FRIDGE?</span>
+            </div>
+          </nav>
+    
+          {/* ── Login Box── */}
+          <section className="signup">
+            <div className="signup-content">
+              <h1 className="signup-heading">Sign Up</h1>
+              <form className="signup-form" onSubmit={handleSubmit}>
+                <label>Email</label>
+                <input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <label>Password</label>
+                <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <label>Confirm Password</label>
+                <input
+                type="password"
+                placeholder="Confirm your password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+                />
+                <label>{error && <p className="error-text">{error}</p>}</label>
+
+                <button className="signup-btn" type="submit">
+                    Sign Up
+                </button>
+                <label>Already have an account? {"  "}
+                    <Link to="/login" className="login-text">Login</Link>
+                </label>
+              </form>
+            </div>
+          </section>
+        </div>
+      );
+    }
+    
+    export default SignupPage;
